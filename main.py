7af23a9bfd33d4eb9b31c5dab4664bd2421d6167
Alexander <aleks.aleksorion@gmail.com>
2023-12-17 19:17:21 +0300

    binary search test code

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+kek = [1, 2, 3, 4, 5, 6, 7, 8, 12, 14, 17, 22, 33, 34, 37, 78, 88, 89, 92, 100]
 
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = round((low + high) / 2)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+result = binary_search(kek, 22)
+print("Index of element is " + str(result))
